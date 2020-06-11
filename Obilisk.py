d84b448841bcf9d5f84625faec8f2846a0dd149c
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2020-06-10 22:07:54 -0400

    Created new file Obilisk to replace BEMDAS_algo eventually.

---
+++ Obilisk.py
@@ -0,0 +1,29 @@
+from math_core.Equation import Equation
+from math_core.Arithmetic import Arithmetic
+
+from typing import List, Tuple, Union
+
+def decode(input_eqn: str) -> Tuple[Equation, str]:
+
+    eqn = Equation(input_eqn)
+    if eqn.var_type[0] == "":
+
+        return eqn, "compute"
+
+    elif len(eqn.var_type) > 1:
+
+        #raise NotImplementedError("Multivariable problems are not yet supported.")
+        eqn.solution.append("Multivariable problems are not yet supported.")
+        return eqn, "not supported"
+
+    else:
+
+        return eqn, "solve"
+
+def compute(input: Equation) -> Tuple[List[str], List[Union[int, float, complex]]]:
+
+    ans=[]
+    eqn = Arithmetic(input.eqn_string)
+    ans.append(eqn.calculate())
+
+    return eqn.solution, ans
