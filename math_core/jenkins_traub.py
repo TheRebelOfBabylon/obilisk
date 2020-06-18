c9d8a1cef40a5fc1fa209f34c55e5f08cb51fc13
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2020-06-17 21:25:25 -0400

    Readjusting Jenkins-Traub algo

--- math_core/jenkins_traub.py
+++ math_core/jenkins_traub.py
@@ -2,13 +2,15 @@ import math
 import cmath
 import random
 from math_core.Calculus import Calculus
+from math_core.Algebra import poly_add
 
 from typing import List, Union, Tuple
 
 def real_poly(eqn: Calculus) -> List[Union[int, float, complex]]:
 	"""Top level function which calls the rpoly, Jenkins-Traub algorithm."""
 	num_roots = eqn.deg[0]
-	ans = []			
+	ans = []
+	var = eqn.var_type[0]
 
 	#Fundamental theorem of algebra says number of highest exponent is the number of roots
 	i = 0
@@ -26,38 +28,48 @@ def real_poly(eqn: Calculus) -> List[Union[int, float, complex]]:
 		new_eqn = eqn.lin_divide(divisor)
 		del new_eqn[-1]
 		#print(new_eqn.eqn)
-		#TODO create a new method in algebra that updates all attributes when given a coeff type array
+		eqn.reset_params()
 		eqn.coeff = new_eqn
-		#eqn.update_attributes()
+		eqn.var_type = [var]
+		eqn.update_params_from_coeff()
 
 	return ans
 
 def rpoly(eqn: Calculus) -> Union[int, float, complex]:
 	"""RPOLY Jenkins-Traub algorithm for polynomial root finding."""
-	coeff = eqn
-	coeff.coeff = coeff.normalize()
-	#coeff.update_attributes()
+	coeff = Calculus()
+	coeff.coeff = eqn.normalize()
+	coeff.var_type = eqn.var_type
+	coeff.update_params_from_coeff()
 	#print(coeff.eqn)
 
 	#Stage 1: No-shift process. Assuming M = 5
 
-	#TODO - Change Equation constructor so that the input string is optional
-	K = coeff.coeff_derivative()
+	K = Calculus()
+	K.coeff = coeff.coeff_derivative()
+	K.var_type = coeff.var_type
+	K.update_params_from_coeff()
 	#print(K.eqn)
 
 	for i in range(0,5):
 
 		constant = -1*K.evaluate(0)/coeff.evaluate(0)
 		#print("Constant is",constant)
-		P_z = coeff.poly_multiply(constant)
+		P_z = Calculus()
+		P_z.coeff = coeff.poly_multiply(constant)
 		#print("P_z",P_z.eqn)
-		K_prime = Algebra.poly_add(K.coeff,P_z.coeff)
+		K_prime = Calculus()
+		K_prime.coeff = poly_add(K.coeff,P_z.coeff)
 		#print("K_prime",K_prime.eqn)
 		divisor = [1,0]
-		new_K = K_prime.lin_divide(divisor)
+		new_K = Calculus()
+		new_K.coeff = K_prime.lin_divide(divisor)
 		del new_K.coeff[-1]
 		#print("new_K",new_K.eqn)
-		K = new_K
+		K.reset_params()
+		K.coeff = new_K.coeff
+		K.var_type = coeff.var_type
+		K.update_params_from_coeff()
 		#print(i)
 
 	#Stage 2: Fixed-shift Process
@@ -76,14 +88,16 @@ def rpoly(eqn: Calculus) -> Union[int, float, complex]:
 			for i in range(0,100):
 				
 				constant = -1*K.evaluate(s)/coeff.evaluate(s)
-				P_z = coeff.poly_multiply(constant)
-				K_prime = Algebra.poly_add(K.coeff,P_z.coeff)
+				P_z.coeff = coeff.poly_multiply(constant)
+				K_prime.coeff = poly_add(K.coeff,P_z.coeff)
 				divisor = [1,-1*s]
-				new_K = K_prime.lin_divide(divisor)
+				new_K.coeff = K_prime.lin_divide(divisor)
 				del new_K.coeff[-1]
 
-				K_bar = K.normalize()
-				new_K_bar = new_K.normalize()
+				K_bar = Calculus()
+				K_bar.coeff = K.normalize()
+				new_K_bar = Calculus
+				new_K_bar.coeff = new_K.normalize()
 
 				t_curr = s-(coeff.evaluate(s)/K_bar.evaluate(s))
 				t_new = s-(coeff.evaluate(s)/new_K_bar.evaluate(s))
@@ -106,7 +120,7 @@ def rpoly(eqn: Calculus) -> Union[int, float, complex]:
 
 		old_err = coeff.evaluate(s)
 		curr_err = 1
-		K_bar = K.normalize()
+		K_bar.coeff = K.normalize()
 		s = s-(coeff.evaluate(s)/K_bar.evaluate(s))
 		old_s = 0
 		stage_three_success = False
@@ -119,12 +133,12 @@ def rpoly(eqn: Calculus) -> Union[int, float, complex]:
 				break
 
 			constant = -1*K.evaluate(s)/coeff.evaluate(s)
-			P_z = coeff.poly_multiply(constant)
-			K_prime = Algebra.poly_add(K.coeff,P_z.coeff)
+			P_z.coeff = coeff.poly_multiply(constant)
+			K_prime.coeff = poly_add(K.coeff,P_z.coeff)
 			divisor = [1,-1*s]
-			new_K = K_prime.lin_divide(divisor)
+			new_K.coeff = K_prime.lin_divide(divisor)
 			del new_K.coeff[-1]
-			new_K_bar = new_K.normalize()
+			new_K_bar.coeff = new_K.normalize()
 
 			s = s-(coeff.evaluate(s)/new_K_bar.evaluate(s))
 			curr_err = coeff.evaluate(s)
@@ -147,11 +161,12 @@ def rpoly(eqn: Calculus) -> Union[int, float, complex]:
 
 	return s
 	
-def get_random_root(eqn: Algebra) -> Union[int, float, complex]:
+def get_random_root(eqn: Calculus) -> Union[int, float, complex]:
 	"""Function for finding random roots."""
-	cauchy_eqn = eqn.cauchy_poly()
+	cauchy_eqn = Calculus()
+	cauchy_eqn.coeff = eqn.cauchy_poly()
 	#print("Cauchy",cauchy_eqn.eqn)
-	beta = cauchy_eqn.newton_raphson())
+	beta = cauchy_eqn.newton_raphson()
 	#print("beta",beta)
 	rand = random.uniform(0,1)*2*math.pi
 	root = abs(beta)*cmath.exp(1j*rand)
