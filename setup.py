b0de5556f6b3cbdcab1d9983716590ca7d1a6ecb
TheRebelOfBabylon <therebelofbabylon@protonmail.com>
2020-07-24 11:46:34 -0400

    added setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='obilisk',
+    packages=['obilisk'],
+    description='Symbolic Math Library for Python',
+    version='1.0',
+    url='https://github.com/TheRebelOfBabylon/obilis',
+    author='TheRebelOfBabylon',
+    author_email='therebelofbabylon@protonmail.com',
+    keywords=['pip','obilisk']
+    )
